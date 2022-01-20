6053a2bafa5dc457e991c5a346868b4ad4433d75
Joel Voss <mail@joelvoss.com>
2022-01-20 22:39:30 +0100

    feat: rewrite react import (namespace import)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { forwardRef } from 'react';
+import * as React from 'react';
 
 /**
  * VisuallyHidden visually hides child components. It is mostly used to provide
  * text for screen readers in components that otherwise would not include text.
  * It is the logical opposite of the `aria-hidden` attribute.
  */
-export const VisuallyHidden = forwardRef(
+export const VisuallyHidden = React.forwardRef(
 	({ as: Comp = 'span', style = {}, ...props }, ref) => (
 		<Comp
 			ref={ref}
